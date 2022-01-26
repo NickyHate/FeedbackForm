a388778a3d4d3d452c39395ee4c2742a14470efe
Nikita <nikitostokarev22@gmail.com>
2022-01-26 18:24:08 +0300

    first commit

---
+++ webpack.config.js
@@ -0,0 +1,55 @@
+const path = require('path')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const { CleanWebpackPlugin } = require('clean-webpack-plugin')
+
+module.exports = {
+    entry: {
+        main: path.resolve(__dirname, './src/index.js'),
+    },
+    output: {
+        path: path.resolve(__dirname, './dist'),
+        filename: '[name].bundle.js',
+    },
+    mode: 'development',
+    devServer: {
+        historyApiFallback: true,
+        open: true,
+        compress: true,
+        port: 8080,
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            title: 'Feedback form on React',
+            template: path.resolve(__dirname, './src/template.html'), // шаблон
+            filename: 'index.html', // название выходного файла
+        }),
+        new CleanWebpackPlugin(),
+    ],
+    module: {
+        rules: [
+            {
+                test: /\.m?js$/,
+                exclude: /(node_modules|bower_components)/,
+                use: {
+                    loader: "babel-loader"
+                }
+            },
+            {
+                test: /\.css$/,
+                use: [
+                    "style-loader",
+                    {
+                        loader: "css-loader",
+                        options: {
+                            modules: true
+                        }
+                    }
+                ]
+            },
+            {
+                test: /\.(png|svg|jpg|gif)$/,
+                use: ["file-loader"]
+            }
+        ]
+    }
+}
