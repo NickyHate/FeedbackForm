5ea99571f5fa6c61d83eb966f9908571212d2aa1
Nikita <nikitostokarev22@gmail.com>
2022-01-28 03:23:26 +0300

    new commit

--- src/component/form.component.js
+++ src/component/form.component.js
@@ -9,9 +9,24 @@ class Form extends Component {
                 <input type="text" id={s.name} name="user_name"></input>
             </div>
             <div className={s.inputContainer}>
-                <label for="surname">Фамилия:</label>
+                <label for="surname">Компания:</label>
                 <input type="text" id={s.surname} name="user_surname"></input>
             </div>
+            <div className={s.inputContainer}>
+                <label for="email">Email:</label>
+                <input type="text" id={s.email} name="user_email"></input>
+            </div>
+            <div className={s.inputContainer}>
+                <label for="date">Дата:</label>
+                <input type="text" id={s.date} name="user_date"></input>
+            </div>
+            <div className={s.textareaContainer}>
+                <label for="textarea">Введите текст:</label>
+                <textarea id={s.textarea} name="user_textarea"></textarea>
+            </div>  
+            <div className={s.btnContainer}>
+                <input type="submit" id={s.postBtn}></input> 
+            </div>                     
         </form>;
     }
 }
